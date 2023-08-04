67703a321d53afade1890e60168e4b0755b6959f
Faith-Njeri <ngangafaith3@gmail.com>
2023-08-04 19:46:17 +0300

    My first commit

---
+++ printassignment.py/Variables.py
@@ -0,0 +1,18 @@
+#I understand variables
+#I will be using various variables to represent diffrent values
+
+#First variable representation
+#First= "First Variable. Yaay!"
+#print(First)
+
+#Variable assignment
+Var_1 ='Offer great support to someone'
+Var_2='Elevate my view of data analysis'
+Var_3="God's got it!"
+print("   Core Value")
+print(Var_1)
+
+print("   Coding Motivation")
+print(Var_2)
+
+print(Var_3)
---
+++ prints.py
@@ -0,0 +1,8 @@
+#writing my first hello world
+print("Hello World!")
+
+#More sophisticated print statement
+print("I am a girl")
+print("I am 20 years old")
+print("This is very exciting")
+     
